e9a62e871fa0137ca60753028bed9581a5496f13
Yuichiro Smith <contact@yu-smith.com>
2020-01-24 15:42:44 +0900

    ppwalk: fix recursive for -l

--- ppwalk.zsh
+++ ppwalk.zsh
@@ -1,10 +1,11 @@
 function ppwalk () {
+    local login
     [[ "$1" = "-l" ]]&&login=$1&&shift
     pid=${1:-${$}}
     comm=$(ps -o comm= -p $pid | sed -e 's/.*\///g')
     ppid=$(ps -o ppid= -p $pid | tr -d ' ')
     echo $pid $comm
     if [[ "$ppid" != "0" && ! ( "$login" = "-l" && "$comm" = "login" ) ]];then
-        ppwalk $ppid
+        ppwalk $login $ppid
     fi
 }
