e541c4cf321b24b7f01fcb5ef8e97e35270e30a0
Yuichiro Smith <contact@yu-smith.com>
2020-01-24 15:40:00 +0900

    ppwalk: add -l option (stop at login)

--- ppwalk.zsh
+++ ppwalk.zsh
@@ -1,9 +1,10 @@
 function ppwalk () {
+    [[ "$1" = "-l" ]]&&login=$1&&shift
     pid=${1:-${$}}
     comm=$(ps -o comm= -p $pid | sed -e 's/.*\///g')
     ppid=$(ps -o ppid= -p $pid | tr -d ' ')
     echo $pid $comm
-    if [[ "$ppid" != "0" ]];then
+    if [[ "$ppid" != "0" && ! ( "$login" = "-l" && "$comm" = "login" ) ]];then
         ppwalk $ppid
     fi
 }
